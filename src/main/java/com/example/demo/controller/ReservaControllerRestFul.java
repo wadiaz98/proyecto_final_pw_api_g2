a7c49cf5b11b8e31b3caabc99b3eaff825bc67e9
anthony <amnaranjos@uce.edu.ec>
2024-03-03 15:04:19 -0500

    Reportes de reservas

--- src/main/java/com/example/demo/controller/ReservaControllerRestFul.java
+++ src/main/java/com/example/demo/controller/ReservaControllerRestFul.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.IReservaService;
 import com.example.demo.service.IVehiculoService;
 import com.example.demo.service.dto.ReservaDTO;
+import com.example.demo.service.to.ReporteTO;
 
 @RestController
 @RequestMapping("/reservas")
@@ -27,5 +33,13 @@ public class ReservaControllerRestFul {
 	public ResponseEntity<String> reservarVehiculo(@RequestBody ReservaDTO reserva) {
 		this.iReservaService.reservar(reserva.getCedula(), reserva.getPlaca(), reserva.getFechaInicio(), reserva.getFechaFin());
 		return ResponseEntity.status(HttpStatus.OK).body("La reserva del vehiculo "+reserva.getPlaca()+" se ha registrado con exito!");
+	}
+	
+	//3a. Reportes de reservas
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ReporteTO>> obtenerReporte(@RequestBody ReporteTO reporte, @RequestParam LocalDateTime fInicio, @RequestParam LocalDateTime fFin){
+		this.iReservaService.reporte(fInicio, fFin);
+		
+		return null;
 	}
 }
---
+++ src/main/java/com/example/demo/service/to/ReporteTO.java
@@ -0,0 +1,80 @@
+package com.example.demo.service.to;
+
+import java.math.BigDecimal;
+
+public class ReporteTO {
+	private Integer numero;
+	private BigDecimal subtotal;
+	private String estado;
+	private BigDecimal iva;
+	private BigDecimal total;
+	
+	private String cedula;
+	private String apellido;
+	
+	private String placa;
+	private String marca;
+	private String modelo;
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public BigDecimal getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(BigDecimal subtotal) {
+		this.subtotal = subtotal;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public BigDecimal getIva() {
+		return iva;
+	}
+	public void setIva(BigDecimal iva) {
+		this.iva = iva;
+	}
+	public BigDecimal getTotal() {
+		return total;
+	}
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+}
